d181c7a2d04ae038e5e7725109169436c3187e28
Tymon 'TeeJay' Jędryczka <jedryczkatymon@gmail.com>
2023-05-30 22:12:25 +0200

    added aqi data

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let city = document.getElementById('city')
 let temperature = document.getElementById('temperature')
 let condition = document.getElementById('condition')
+let aqi = document.getElementById('aqi')
 let wind_kph = document.getElementById('wind-kph')
 let wind_dir = document.getElementById('wind-dir')
 let pressure = document.getElementById('pressure')
@@ -12,6 +13,7 @@ fetch('http://localhost:8000/')
         city.innerText = data.location.name
         temperature.innerText = `${Math.round(data.current.temp_c)}°C`
         condition.innerText = data.current.condition.text
+        aqi.innerText = `AQI ${data.current.air_quality["us-epa-index"]}`
         wind_kph.innerText = `${data.current.wind_kph}km/h`
         wind_dir.innerText = data.current.wind_dir
         pressure.innerText = `${data.current.pressure_mb}hPa`
