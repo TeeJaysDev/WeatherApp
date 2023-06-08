41dd8b8bcdb13aaef3203113e0b14e7fe7de324c
Tymon 'TeeJay' Jędryczka <jedryczkatymon@gmail.com>
2023-06-08 22:54:42 +0200

    added values to secondary info section

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let city = document.getElementById('city')
 let temperature = document.getElementById('temperature')
 let condition = document.getElementById('condition')
 let aqi = document.getElementById('aqi')
-let wind_kph = document.getElementById('wind-kph')
-let wind_dir = document.getElementById('wind-dir')
+let wind = document.getElementById('wind')
 let pressure = document.getElementById('pressure')
+let valueFeelsLike = document.getElementById('value-feels-like')
+let valueHumidity = document.getElementById('value-humidity')
+let valueUV = document.getElementById('value-uv')
+let valueClouds = document.getElementById('value-clouds')
 
 fetch('http://localhost:8000/')
     .then(response => {return response.json()})
@@ -14,8 +17,11 @@ fetch('http://localhost:8000/')
         temperature.innerText = `${Math.round(data.current.temp_c)}°C`
         condition.innerText = data.current.condition.text
         aqi.innerText = `AQI ${data.current.air_quality["us-epa-index"]}`
-        wind_kph.innerText = `${data.current.wind_kph}km/h`
-        wind_dir.innerText = data.current.wind_dir
+        wind.innerText = `${data.current.wind_kph}km/h ${data.current.wind_dir}`
         pressure.innerText = `${data.current.pressure_mb}hPa`
+        valueFeelsLike.innerText = `${data.current.feelslike_c}°C`
+        valueHumidity.innerText = `${data.current.humidity}%`
+        valueUV.innerText = `${data.current.uv}`
+        valueClouds.innerText = `${data.current.cloud}%`
     })
     .catch(err => console.log(err))
